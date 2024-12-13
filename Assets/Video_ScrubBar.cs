6f97efe0d479be2ea1151a977dc8ded8f7b87176
Gabriel Sarnikov <53214947+GameInSociety@users.noreply.github.com>
2024-12-13 11:50:17 +0100

    video + sound fix & debug

--- Assets/Video_ScrubBar.cs
+++ Assets/Video_ScrubBar.cs
@@ -8,16 +8,55 @@ public class Video_ScrubBar : MonoBehaviour, IDragHandler, IPointerDownHandler, 
     public RectTransform bg_RectTransform;
     public RectTransform fill_RectTransform;
 
-    public float time = 0f;
+    public Slider soundSlider;
+
+    public RectTransform startAnchor;
+    public RectTransform endAnchor;
+
+    public VideoPlayer player;
+
+    public float lerp;
+
+    bool dragging = false;
+
+    private void Update() {
+        if (dragging) {
+
+        } else {
+            float lerp = (float)player.time / (float)player.length;
+            float x = lerp * bg_RectTransform.sizeDelta.x;
+            fill_RectTransform.sizeDelta = new Vector2(x, fill_RectTransform.sizeDelta.y);
+        }
+
+        player.SetDirectAudioVolume(0, soundSlider.value);
+    }
 
     public void OnDrag(PointerEventData eventData) {
-        
+        dragging = true;
+        UpdateVideoTime(eventData.pointerCurrentRaycast.worldPosition);
     }
 
     public void OnPointerDown(PointerEventData eventData) {
+        UpdateVideoTime(eventData.pointerCurrentRaycast.worldPosition);
+    }
+
+    public void UpdateVideoTime(Vector3 pos) {
+        if (player.isPlaying) {
+            player.Pause();
+        }
+        float d = Vector3.Distance(startAnchor.position, endAnchor.position);
+        pos.y = startAnchor.position.y;
+        float f = Vector3.Distance(startAnchor.position, pos);
+        lerp = f / d;
+        lerp = Mathf.Clamp01(lerp);
+        float x = lerp * bg_RectTransform.sizeDelta.x;
+        fill_RectTransform.sizeDelta = new Vector2(x, fill_RectTransform.sizeDelta.y);
+        player.time = player.length * lerp;
     }
 
     public void OnPointerUp(PointerEventData eventData) {
+        dragging = false;
+        player.Play();
     }
 
 }
